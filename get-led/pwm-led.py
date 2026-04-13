eca8f4a1936219de34fa95b40b3a7bfe1a34ef11
Svinogonov <maksimkuckin07@gmail.com>
2026-04-13 10:00:22 +0300

    Add PWM LED script

--- get-led/pwm-led.py
+++ get-led/pwm-led.py
@@ -1,23 +1,18 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-
-led=26
-GPIO.setup(led, GPIO.OUT)
-pwm = GPIO.PWM(led, 200)
-duty = 0.0
+import RPi.GPIO as gp 
+import time 
+gp.setmode(gp.BCM)
+led = 26 
+buttom = 13
+ind = 6 
+gp.setup(led, gp.OUT) 
+gp.setup(ind, gp.IN) 
+st = 1 
+pwm = gp.PWM(led, 200) 
+duty = 0.0 
 pwm.start(duty)
-
-try:
-    while True:
-        pwm.ChangeDuteCycle(duty) 
-        time.sleep(0.05)
-
-        duty +=1.0
-        if duty > 100.0:
-            duty = 0.0
-except KeyboardInterrupt:
-    pass
-finally:
-    pwm.stop()
-    GPIO.cleanup() 
+while True: 
+    pwm.ChangeDutyCycle(duty) 
+    time.sleep(0.05) 
+    duty += 1.0
+    if duty > 100.0: 
+        duty = 0.0
