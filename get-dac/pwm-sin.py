63babfa5478541519c742d90e084e90e37b780b8
Svinogonov <maksimkuckin07@gmail.com>
2026-04-20 11:48:57 +0300

    Add PWM sine generator

---
+++ get-dac/pwm-sin.py
@@ -0,0 +1,29 @@
+import pwm_dac as pwm 
+import signal_generator as sg 
+import time
+
+amp = 2.7 
+freq = 10 
+fs = 1000 
+vmax = 3.29 
+fpwm = 20000
+
+if amp > vmax: 
+    raise ValueError("Амплитуда сигнала больше динамического диапазона PWM DAC")
+
+try: 
+    dac = pwm.PWM_DAC(12, fpwm, vmax, False)
+
+    t0 = time.perf_counter()
+
+    while True:
+        t = time.perf_counter() - t0
+        v = amp * sg.get_sin_wave_amplitude(freq, t)
+
+        dac.set_voltage(v)
+        sg.wait_for_sampling_period(fs)
+except KeyboardInterrupt: 
+    pass
+
+finally: 
+    dac.deinit()
