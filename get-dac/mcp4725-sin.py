505495cdd1d7c12979841ca89439b4a5048fe612
Svinogonov <maksimkuckin07@gmail.com>
2026-04-20 12:06:23 +0300

    Add MCP4725 sine generator

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,28 @@
+import mcp4725_driver as mcp 
+import signal_generator as sg 
+import time
+
+amp = 2.5 
+freq = 10 
+fs = 1000 
+vmax = 5.11
+
+if amp > vmax: 
+    raise ValueError("Амплитуда сигнала больше динамического диапазона 12-bit DAC")
+
+try: 
+    dac = mcp.MCP4725(vmax, 0x61, False)
+
+    t0 = time.perf_counter()
+
+    while True:
+        t = time.perf_counter() - t0
+        v = amp * sg.get_sin_wave_amplitude(freq, t)
+
+        dac.set_voltage(v)
+        sg.wait_for_sampling_period(fs)
+except KeyboardInterrupt: 
+    pass
+
+finally: 
+    dac.deinit()
